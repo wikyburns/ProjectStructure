8a9387266a07e42024e9b51ad0f1d4688bbfe7a8
Sebastian Bejenaru <sebasbeje@gmail.com>
2023-03-02 09:41:49 +0100

    Initial Commit

---
+++ StructureTest/StructureTest.h
@@ -0,0 +1,18 @@
+//
+//  StructureTest.h
+//  StructureTest
+//
+//  Created by Sebas Bejenaru on 2/3/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StructureTest.
+FOUNDATION_EXPORT double StructureTestVersionNumber;
+
+//! Project version string for StructureTest.
+FOUNDATION_EXPORT const unsigned char StructureTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StructureTest/PublicHeader.h>
+
+
